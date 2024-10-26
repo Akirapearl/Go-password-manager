ce9621da1b9dcc9af524edd85061dad6242a3325
Akirapearl <draculaspot@hotmail.com>
2024-10-26 11:29:38 +0200

    Minor improve - Spacing between lines - for loop

--- main.go
+++ main.go
@@ -27,13 +27,28 @@ func run() error {
 	fmt.Println("=== Current content of your file ===")
 	read.ReadFile(path)
 
+	//Check file - then clear the screen
 	time.Sleep(5 * time.Second)
 	fmt.Print("\033[;1H\033[2J")
-	// Write single line to file
-	change.WriteLine(path)
 
-	fmt.Println("=== Updated content of your file - Insert ===")
-	read.ReadFile(path)
+	for {
+		// Write single line to file
+		change.WriteLine(path)
+
+		fmt.Println("=== Updated content of your file - Insert ===")
+		fmt.Printf("\n")
+		read.ReadFile(path)
+
+		fmt.Println("\n Do you want to enter more data?[y/n]")
+
+		var respData string
+		fmt.Scan(&respData)
+
+		if respData != "y" && respData != "Y" {
+			break
+		}
+		fmt.Print("\033[;1H\033[2J")
+	}
 	/*---WARNING---*/
 	// Every single line of code needs to be above the return nil line below
 	// Otherwise it will return unreachable code
