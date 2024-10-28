9f2cecbc4fbc2a986508c2cd75e90843e9df1fff
Akirapearl <draculaspot@hotmail.com>
2024-10-28 20:19:32 +0100

    Update register - Working statement - Pending real modification

--- change/update.go
+++ change/update.go
@@ -2,6 +2,22 @@ package change
 
 import "fmt"
 
+func switchValidate(v int, old *[3]string, new string) string {
+	switch v {
+	case 0:
+		old[0] = new
+		fmt.Printf("New value is: %v", new)
+	case 1:
+		old[1] = new
+		fmt.Printf("New value is: %v", new)
+	case 2:
+		old[2] = new
+		fmt.Printf("New value is: %v", new)
+	default:
+		fmt.Println("The number is wrong!")
+	}
+	return "none"
+}
 func UpdateLine(file string) {
 	// Ask for values to check
 	fmt.Println("Update mode - Introduce your username")
@@ -15,8 +31,15 @@ func UpdateLine(file string) {
 	fmt.Println("Finally, introduce your password")
 	fmt.Scan(&dataUpdate[2])
 
-	// Validate if field exists
+	// Validate/Verify if field exists
+	fmt.Println("Which field do you want to change [0/1/2]?", dataUpdate[0], dataUpdate[1], dataUpdate[2])
+	var updatePrompt int
+	fmt.Scan(&updatePrompt)
 
-	fmt.Println("Which field do you want to change?", dataUpdate[0], dataUpdate[1], dataUpdate[2])
+	fmt.Println("Which value would you like to assign to that field? ")
+	var newPrompt string
+	fmt.Scan(&newPrompt)
 
+	//Pass value alongside the content of the array
+	switchValidate(updatePrompt, &dataUpdate, newPrompt)
 }
